7ef7639d98932defa53863bd77c0c223a2bf3d38
PixelPips <pips@pixelbyte.net>
2015-03-23 17:29:17 -0600

    v. 0.6
    + Added proper support for multiple errors.
    + Made it so that upload.php will redirect you if accessed directly
    + upload.php will now give you a link the the uploaded file
    + upload.php will tell you how large your file is now, if there's an
    error.
    
    * Fixed the Byte conversion in upload. Maximum file size is now properly
    working.

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 // @TODO: Config page. Probably gunna need to use SQL allow to store and modify these vairiables outside the script.
 
 
-$file_size_limit = "50"; // the maximum file size in MB.
+$file_size_limit = "30"; // the maximum file size in MB.
 
-$allowed_directories = ["test", "img"]; // the directories that can have a file uploaded to.
+$allowed_directories = ["test1", "test2"]; // the directories that can have a file uploaded to.
 
 $administrator_PIN = "1234"; // the PIN that authenticate the upload.
 
--- upload.php
+++ upload.php
@@ -4,32 +4,35 @@
 
 include './config.php';
 
+if(!isset($_FILES["file"])){
+  header('Location: ./');
+}
+
 
 $target_directory = getcwd() . "/" . $_POST["directory"] . "/"; // The target directory to put the file
 $pin = $_POST["pin"]; // The admin pin to allow upload
 $file = $_FILES["file"]; // The file that's being uploaded
 $file_name = $_FILES["file"]["name"]; // The name of the file being uploaded (Example: test.png)
 $file_type = $_FILES["file"]["type"]; // File type.
-$file_size = intval( $_FILES["file"]["size"]) / 1024; // Convert from KB to MB
+$file_size = (intval( $_FILES["file"]["size"]) / 1024 / 1024); // Convert from Bytes to MB
 $target_file = $target_directory . $file_name; // The more compiled target file/location. (Example: /folder/file.ext)
 $upload_OK = true; // The upload boolean. If there's a problem, this goes false and the file is discarded.
-$error_message = ""; // The error message, is set if $upload_OK == true;
-
+$error_messages = []; // The error message, is set if $upload_OK == false;
 
 // Check if the file exists. We don't want to delete/override a file.
 if (file_exists($target_file)){
     
     $upload_OK = false;
-    $error_message = $target_file . " already exists. Rename it and try again.";
+    array_push($error_messages, $target_file . " already exists. Rename it and try again.");
     
 }
 
 
 // Check file size.
-if ($file_size > $file_size_limit){
+if ($file_size > intval($file_size_limit)){
     
     $upload_OK = false;
-    $error_message = $file_name . " excedes the " . $file_size_limit . "MB limit.";
+     array_push($error_messages, $file_name . " excedes the " . $file_size_limit . "MB limit. The file size is " . $file_size . "MB.");
     
 }
 
@@ -42,7 +45,7 @@ if(!file_exists($target_directory)){
     }else{
         
         $upload_OK = false;
-        $error_message = "Quit fucking arround. I know you're trying to upload a file to a directory that doesn't exist.";
+       array_push($error_messages,  "Quit fucking arround. I know you're trying to upload a file to a directory that doesn't exist.");
     }
     
 }
@@ -51,14 +54,14 @@ if(!file_exists($target_directory)){
 if($pin != $administrator_PIN){
     
     $upload_OK = false;
-    $error_message = "You entered the wrong PIN.";
+     array_push($error_messages, "You entered the wrong PIN.");
     
 }
 
 if( ! (isset($_POST["directory"]) and isset($_POST["pin"]) and isset($_FILES["file"]))){
     
     $upload_OK = false;
-    $error_message = "Please go to the main page.";
+    array_push($error_messages,"Please go to the main page.");
     
 }
 
@@ -66,12 +69,16 @@ if( ! (isset($_POST["directory"]) and isset($_POST["pin"]) and isset($_FILES["fi
 
 if(!$upload_OK){
     
-    echo ("Error: " . $error_message);
+    echo ("One or more errors have occured: <br>");
+  
+  foreach($error_messages as $error){
+  echo("- " . $error . "<br>");
+  }
     
 } else{
     
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-        echo "The file was uploaded.";
+        echo "The file was uploaded. at <a href='". $_POST["directory"] . "/" . $file_name ."'>".$_POST["directory"] . "/" .$file_name."</a>";
     } else {
         echo "Unknown error.";
     }
